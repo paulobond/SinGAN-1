2284a8625586c20432af50d493f8dbc9eeb8134c
Paul Bonduelle <paul@feedly.com>
2020-01-03 15:11:55 +0100

    wip

--- main_train.py
+++ main_train.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--inpainting_mask_size', type=int, help='Random mask (inpainting)', default=0)
 
-    parser.add_argument('--half_rec_loss', type=bool, help='Rec loss computed on the right half only (used as a test)',
+    parser.add_argument('--half_rec_loss', type=bool, help='Rec loss computed on the left half only (used as a test)',
                         default=False)
 
     opt = parser.parse_args()
@@ -27,6 +27,9 @@ if __name__ == '__main__':
     reals = []  # ranged from most downsampled to the original image
     NoiseAmp = []
     dir2save = functions.generate_dir2save(opt)
+
+    if opt.half_rec_loss:
+        print(f"EXPERIMENT: rec loss computed on the left half of the image only")
 
     if (os.path.exists(dir2save)):
         print('trained model already exist')
